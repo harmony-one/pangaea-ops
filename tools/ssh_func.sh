277c85338b15f87d088451613d119261d353cf2b
Eugene Kim <ek@harmony.one>
2019-12-07 11:16:36 +0000

    Add SCP

--- tools/ssh_func.sh
+++ tools/ssh_func.sh
@@ -1,2 +1,3 @@
 PSSH() { pssh -p 300 -x '-F/data/pangaea-ops/tools/ssh_config' "$@"; }
 SSH() { ssh -F /data/pangaea-ops/tools/ssh_config "$@"; }
+SCP() { scp -F /data/pangaea-ops/tools/ssh_config "$@"; }
