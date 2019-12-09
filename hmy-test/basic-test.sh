a439c4f76c60f1b0267f0c5dda82bb56425562a7
soph <sophoah@yahoo.fr>
2019-12-10 00:01:47 +0800

    fxing hmy bin path

--- hmy-test/basic-test.sh
+++ hmy-test/basic-test.sh
@@ -9,11 +9,11 @@ SHUNITPATH="../../shunit2/shunit2"
 
 #path to hmy binary
 HMYCLI_ABSOLUTE_FOLDER="/root/hmy-devnet"
-
+HMYCLIBIN="hmy"
 
 test_HMY_version() {
   #note the current hmy version v138 shows the version in the stderr
-  output=$((${HMYCLIPATH} version) 2>&1)
+  output=$((${HMYCLIBIN} version) 2>&1)
   returncode=$?
   assertEquals 'Testing error code of hmy version which should be 0' "0" "${returncode}"
   assertContains 'Testing hmy version' "${output}" 'v138-3ef3b5c'
@@ -21,14 +21,14 @@ test_HMY_version() {
 }
 
 test_HMY_Check_Balance() {
-  output=$(${HMYCLIPATH} --node="https://api.s1.b.hmny.io/" balances one1yc06ghr2p8xnl2380kpfayweguuhxdtupkhqzw | jq ".[0].amount")
+  output=$(${HMYCLIBIN} --node="https://api.s1.b.hmny.io/" balances one1yc06ghr2p8xnl2380kpfayweguuhxdtupkhqzw | jq ".[0].amount")
   returncode=$?
   assertEquals 'Testing error code of hmy balance check which should be 0' "0" "${returncode}"
   assertEquals "testing balance above 0 for one1yc06ghr2p8xnl2380kpfayweguuhxdtupkhqzw in pangaea" "1" "$(echo "${output} > 0" | bc -l)"
 }
 
 test_HMY_Known_Chain() {
-  output=$(${HMYCLIPATH} blockchain known-chains --no-pretty)
+  output=$(${HMYCLIBIN} blockchain known-chains --no-pretty)
   returncode=$?
   assertEquals 'Testing error code of hmy known chain test which should be 0' "0" "${returncode}"
   assertEquals '["mainnet","testnet","devnet"]' "${output}"
