cbbd7a94c1ff8f862af39431ba42eb2df595fc17
soph <sophoah@yahoo.fr>
2019-12-15 21:27:41 +0800

    adding config file, test files are sourcing it

--- hmy-test/staking-test.sh
+++ hmy-test/staking-test.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 # Author: P-OPS soph 
 
-#absolute path of the harmony folder
-HARMONYPATH="/root/hmy-devnet/"
-
-#path to shunit2:
-SHUNITPATH=${HARMONYPATH}"shunit2/shunit2"
-
-#path to hmy binary
-HMYCLI_ABSOLUTE_FOLDER=${HARMONYPATH}
-HMYCLIBIN="./hmy"
-
-#chain-id this test file is supposed run against
-#available chain-id
-#[ #As of 7 Dec 2019
-#  "mainnet",
-#  "testnet", ==> pangaea/testnet
-#  "devnet" ==> devnet
-#]
-chainid="devnet"
-
-#api endpoints
-#Pangaea: --node=https://api.s0.p.hmny.io
-#Devnet: --node=https://api.s0.pga.hmny.io
-apiendpoint="localhost:9500"
-
+source config.sh
 
 # and import it so hmy can use it in all the below command
 # ./hmy keys import-ks /root/.hmy/keystore/UTC--2019-11.....
